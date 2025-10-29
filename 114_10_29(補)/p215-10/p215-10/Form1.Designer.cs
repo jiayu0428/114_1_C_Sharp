2548c5e7a761df775cde562aacf5a9e61fb80649
jiayu0428 <a114221065@mail.shu.edu.tw>
2025-10-29 14:17:05 +0800

    114_10_29(補)

---
+++ 114_10_29(補)/p215-10/p215-10/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace p215_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bananaPictureBox = new System.Windows.Forms.PictureBox();
+            this.applePictureBox = new System.Windows.Forms.PictureBox();
+            this.limePictureBox = new System.Windows.Forms.PictureBox();
+            this.pearPictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bananaPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.applePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.limePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pearPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bananaPictureBox
+            // 
+            this.bananaPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("bananaPictureBox.Image")));
+            this.bananaPictureBox.Location = new System.Drawing.Point(121, 77);
+            this.bananaPictureBox.Name = "bananaPictureBox";
+            this.bananaPictureBox.Size = new System.Drawing.Size(140, 120);
+            this.bananaPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bananaPictureBox.TabIndex = 0;
+            this.bananaPictureBox.TabStop = false;
+            // 
+            // applePictureBox
+            // 
+            this.applePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("applePictureBox.Image")));
+            this.applePictureBox.Location = new System.Drawing.Point(267, 77);
+            this.applePictureBox.Name = "applePictureBox";
+            this.applePictureBox.Size = new System.Drawing.Size(140, 120);
+            this.applePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.applePictureBox.TabIndex = 1;
+            this.applePictureBox.TabStop = false;
+            // 
+            // limePictureBox
+            // 
+            this.limePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("limePictureBox.Image")));
+            this.limePictureBox.Location = new System.Drawing.Point(121, 250);
+            this.limePictureBox.Name = "limePictureBox";
+            this.limePictureBox.Size = new System.Drawing.Size(140, 120);
+            this.limePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.limePictureBox.TabIndex = 2;
+            this.limePictureBox.TabStop = false;
+            // 
+            // pearPictureBox
+            // 
+            this.pearPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pearPictureBox.Image")));
+            this.pearPictureBox.Location = new System.Drawing.Point(267, 250);
+            this.pearPictureBox.Name = "pearPictureBox";
+            this.pearPictureBox.Size = new System.Drawing.Size(140, 120);
+            this.pearPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pearPictureBox.TabIndex = 3;
+            this.pearPictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(118, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 47);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "115 calories";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(267, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 47);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "80 calories";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(264, 373);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 47);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "120 calories";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(118, 373);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 47);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "90 calories";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(456, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(139, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "total calories";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.totalLabel.Location = new System.Drawing.Point(450, 89);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(158, 52);
+            this.totalLabel.TabIndex = 9;
+            this.totalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(460, 250);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(145, 52);
+            this.resetButton.TabIndex = 10;
+            this.resetButton.Text = "reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(460, 318);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(145, 52);
+            this.exitButton.TabIndex = 11;
+            this.exitButton.Text = "exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pearPictureBox);
+            this.Controls.Add(this.limePictureBox);
+            this.Controls.Add(this.applePictureBox);
+            this.Controls.Add(this.bananaPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bananaPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.applePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.limePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pearPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bananaPictureBox;
+        private System.Windows.Forms.PictureBox applePictureBox;
+        private System.Windows.Forms.PictureBox limePictureBox;
+        private System.Windows.Forms.PictureBox pearPictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
